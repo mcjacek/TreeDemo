81704b7a976b748580424611c53810f1324bc53e
mcjacek <mcjacek@gmail.com>
2018-03-27 14:48:58 +0200

    Tree Demo demonstrating basic Tree data structure with Family example

---
+++ TreeDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var family = new TreeNode("Alfred")
+            {
+                new TreeNode("Aleksander")
+                {
+                    new TreeNode("Jacek"),
+                    new TreeNode("Wojciech")
+                    {
+                        new TreeNode("Wiktoria")
+                    }
+                },
+                new TreeNode("Witold")
+                {
+                    new TreeNode("Tomasz"),
+                    new TreeNode("Beata")
+                }
+            };
+
+            //var family= new TreeNode("Alfred");
+            //    var tata = new TreeNode("Aleksander");
+            //   .Add(tata);
+            //        var jacek = new TreeNode("Jacek");
+            //        var wojciech = new TreeNode("Wojciech");
+            //        tata.Add(jacek);
+            //        tata.Add(wojciech);
+            //            var wiktoria = new TreeNode("Wiktoria");
+            //            wojciech.Add(wiktoria);
+
+            //var wujek = new TreeNode("Witold");
+            //   .Add(wujek);
+            //        var tomasz = new TreeNode("Tomasz");
+            //        var beata = new TreeNode("Beata");
+            //        wujek.Add(tomasz);
+            //        wujek.Add(beata);
+
+            var treeString = family.GetDataString();
+        }
+    }
+}
---
+++ TreeDemo/TreeNode.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeDemo
+{
+    class TreeNode : IEnumerable<TreeNode>
+    {
+        private readonly Dictionary<string, TreeNode> _children =
+                                            new Dictionary<string, TreeNode>();
+
+        public readonly string ID;
+        public TreeNode Parent { get; private set; }
+
+        public TreeNode(string id)
+        {
+            this.ID = id;
+        }
+        public TreeNode GetChild(string id)
+        {
+            return this._children[id];
+        }
+
+        public void Add(TreeNode item)
+        {
+            if (item.Parent != null)
+            {
+                item.Parent._children.Remove(item.ID);
+            }
+
+            item.Parent = this;
+            this._children.Add(item.ID, item);
+        }
+
+        public IEnumerator<TreeNode> GetEnumerator()
+        {
+            return this._children.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public int Count
+        {
+            get { return this._children.Count; }
+        }
+
+        string stringToReturn = string.Empty;
+        public string GetDataString()
+        {
+            stringToReturn = "";
+            BuildTree(this, 0);
+
+            return stringToReturn;
+        }
+
+        private void BuildTree(TreeNode node, int depth)
+        {
+            stringToReturn +=  "".PadLeft(depth * 3) + node.ID + System.Environment.NewLine;
+
+            foreach (var child in node._children)
+            {
+                BuildTree(child.Value, depth + 1);
+            }
+        }
+    }
+}
