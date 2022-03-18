c24fd9cc08d6c6963535e8397b923afc17fd91a4
Jacek Macura <jacek.macura@gmail.com>
2022-03-18 11:59:00 +0100

    Updated family tree

--- TreeDemo/Program.cs
+++ TreeDemo/Program.cs
@@ -10,39 +10,50 @@ namespace TreeDemo
     {
         static void Main(string[] args)
         {
-            var family = new TreeNode("Alfred")
-            {
-                new TreeNode("Aleksander")
-                {
-                    new TreeNode("Jacek"),
-                    new TreeNode("Wojciech")
-                    {
-                        new TreeNode("Wiktoria")
-                    }
-                },
-                new TreeNode("Witold")
-                {
-                    new TreeNode("Tomasz"),
-                    new TreeNode("Beata")
-                }
-            };
+            //var family = new TreeNode("Alfred")
+            //{
+            //    new TreeNode("Aleksander")
+            //    {
+            //        new TreeNode("Jacek")
+            //        {
+            //            new TreeNode("Adam"),
+            //            new TreeNode("Eryk")
+            //        },
+            //        new TreeNode("Wojciech")
+            //        {
+            //            new TreeNode("Wiktoria"),
+            //            new TreeNode("Kajetan")
+            //        }
+            //    },
+            //    new TreeNode("Witold")
+            //    {
+            //        new TreeNode("Tomasz"),
+            //        new TreeNode("Beata")
+            //    }
+            //};
 
-            //var family= new TreeNode("Alfred");
-            //    var tata = new TreeNode("Aleksander");
-            //   .Add(tata);
-            //        var jacek = new TreeNode("Jacek");
-            //        var wojciech = new TreeNode("Wojciech");
-            //        tata.Add(jacek);
-            //        tata.Add(wojciech);
-            //            var wiktoria = new TreeNode("Wiktoria");
-            //            wojciech.Add(wiktoria);
+            var family = new TreeNode("Alfred");
+            var tata = new TreeNode("Aleksander");
+            family.Add(tata);
+            var jacek = new TreeNode("Jacek");
+            var wojciech = new TreeNode("Wojciech");
+            tata.Add(jacek);
+            var adam = new TreeNode("Adam");
+            var eryk = new TreeNode("Eryk");
+            jacek.Add(adam);
+            jacek.Add(eryk);
+            tata.Add(wojciech);
+            var wiktoria = new TreeNode("Wiktoria");
+            var kajetan = new TreeNode("Kajetan");
+            wojciech.Add(wiktoria);
+            wojciech.Add(kajetan);
 
-            //var wujek = new TreeNode("Witold");
-            //   .Add(wujek);
-            //        var tomasz = new TreeNode("Tomasz");
-            //        var beata = new TreeNode("Beata");
-            //        wujek.Add(tomasz);
-            //        wujek.Add(beata);
+            var wujek = new TreeNode("Witold");
+            family.Add(wujek);
+            var tomasz = new TreeNode("Tomasz");
+            var beata = new TreeNode("Beata");
+            wujek.Add(tomasz);
+            wujek.Add(beata);
 
             var treeString = family.GetDataString();
         }
